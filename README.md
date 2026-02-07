6282e1e9ecaab0fd4318bda57ae511e373115c09
JacekAGD <jacekklimczykkrk@gmail.com>
2026-02-07 10:32:58 +0100

    Update scraper to follow Sirius overview redirects

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 dla zabawy
 
-## Okta redirect flow (PHP)
+## Sirius Partner redirect flow (PHP)
 
-Skrypt `scraper.php` wykonuje żądanie do podanego URL `authorize`, podąża za przekierowaniami i wyświetla wynik końcowy.
-Jeżeli w treści odpowiedzi pojawi się link do bookmarka Okta, skrypt otworzy go i wypisze wynik końcowy tego wywołania.
+Skrypt `scraper.php` wykonuje żądanie do strony `https://sirius-partner.beko.com/overview`,
+podąża za przekierowaniami i wyświetla wynik końcowy (po wszystkich redirectach).
 
 ### Wymagania
 
@@ -14,8 +14,8 @@ Jeżeli w treści odpowiedzi pojawi się link do bookmarka Okta, skrypt otworzy 
 ### Zmienne środowiskowe
 
 ```bash
-# opcjonalnie (możesz podmienić URL authorize)
-export OKTA_AUTHORIZE_URL="https://arcelik.okta-emea.com/oauth2/v1/authorize?..."
+# opcjonalnie (możesz podmienić URL startowy)
+export SIRIUS_OVERVIEW_URL="https://sirius-partner.beko.com/overview"
 
 # opcjonalnie
 export OKTA_USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
@@ -30,4 +30,3 @@ php scraper.php
 ### Uwagi
 
 - Skrypt tylko podąża za przekierowaniami i wypisuje wynik końcowy. Nie zapisuje ani nie używa loginu/hasła.
-- Jeżeli w odpowiedzi pojawi się link do bookmarka Okta `https://arcelik.okta-emea.com/home/bookmark/0oagda9obfeM9qqGs0i7/2557`, skrypt wywoła go i wyświetli wynik po przekierowaniach.
--- scraper.php
+++ scraper.php
@@ -72,23 +72,13 @@ function output_response(array $response, string $requestUrl, string $method): v
     echo "==== END HTTP RESPONSE ====" . "\n";
 }
 
-$authorizeUrl = getenv('OKTA_AUTHORIZE_URL') ?: 'https://arcelik.okta-emea.com/oauth2/v1/authorize?client_id=okta.2b1959c8-bcc0-56eb-a589-cfcfb7422f26&code_challenge=9IlGS3_z4zBobCUX8CJVrCAqwBC1Jem_zgd1O_Q6i5M&code_challenge_method=S256&nonce=FohnrB183nCCvrqAIfjvqnGeQq0vU3lg0SdZjr6qoTsrVlOIkWd9zqxULzqsH8Wn&redirect_uri=https%3A%2F%2Farcelik.okta-emea.com%2Fenduser%2Fcallback&response_type=code&state=UjRLF40ysGs5CvPziZCzk8OktoAUNILDchaBDxBHE1wwOt2nbPRdlGVBI9ZRb8da&scope=openid%20profile%20email%20okta.users.read.self%20okta.users.manage.self%20okta.internal.enduser.read%20okta.internal.enduser.manage%20okta.enduser.dashboard.read%20okta.enduser.dashboard.manage%20okta.myAccount.sessions.manage%20okta.internal.navigation.enduser.read';
-$bookmarkUrl = 'https://arcelik.okta-emea.com/home/bookmark/0oagda9obfeM9qqGs0i7/2557';
+$startUrl = getenv('SIRIUS_OVERVIEW_URL') ?: 'https://sirius-partner.beko.com/overview';
 $userAgent = getenv('OKTA_USER_AGENT') ?: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
 $cookieJar = sys_get_temp_dir() . '/okta_cookie_' . bin2hex(random_bytes(8)) . '.txt';
 
-$authorizeResponse = http_request('GET', $authorizeUrl, [
+$finalResponse = http_request('GET', $startUrl, [
     'User-Agent: ' . $userAgent,
     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 ], null, true, $cookieJar);
 
-if (strpos($authorizeResponse['body'], $bookmarkUrl) !== false) {
-    $bookmarkResponse = http_request('GET', $bookmarkUrl, [
-        'User-Agent: ' . $userAgent,
-        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    ], null, true, $cookieJar);
-    output_response($bookmarkResponse, $bookmarkUrl, 'GET');
-    exit(0);
-}
-
-output_response($authorizeResponse, $authorizeUrl, 'GET');
+output_response($finalResponse, $startUrl, 'GET');
